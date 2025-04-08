5677866971ce949d6559f66ab2605d1f20004111
xiii <kirill.popov@icloud.com>
2025-04-08 19:48:40 +0300

    add class name output

--- src/app/Main.java
+++ src/app/Main.java
@@ -24,6 +24,8 @@ public class Main {
 
         System.out.println("--------------------------------");
 
+        System.out.println("Class: " + arrayUtils.getClass().getCanonicalName());
+        System.out.println("*********************************");
         for (Method m : arrayUtils.getClass().getDeclaredMethods()) {
             if (m.isAnnotationPresent(MethodInfo.class)) {
                 MethodInfo methodInfo = m.getAnnotation(MethodInfo.class);
